4830d8d14cdc71c3129b60b2998ef413d958077d
YobieYo <YobieYo2@gmail.com>
2024-10-21 11:09:34 +0300

    Update z1.py

--- Лабораторная работа 1/z1.py
+++ Лабораторная работа 1/z1.py
@@ -3,6 +3,6 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 n=0
 for i in range(len(numbers)):
     if numbers[i]==None: n=i
-average=[sum(numbers[:4]+numbers[5:])/len(numbers)]
-new_numbers=numbers[:4]+average+numbers[5:]
+average=[sum(numbers[:n]+numbers[(n+1):])/len(numbers)]
+new_numbers=numbers[:n]+average+numbers[(n+1):]
 print("Измененный список:", new_numbers)
